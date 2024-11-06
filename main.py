21afdad9a198844938f20848d96cd1861c16c5a3
1ikeadragon <40909973+1ikeadragon@users.noreply.github.com>
2024-11-06 10:56:04 +0530

    ???

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import subprocess
 
 API = "https://dogbolt.org/api/binaries/"
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-DECOMPILERS = {"binja": "BinaryNinja", "angr": "angr", "ghidra": "Ghidra", "hexrays": "Hex-Rays"}
+DECOMPILERS = {"binja": "BinaryNinja", "angr": "angr", "ghidra": "Ghidra", "ida": "Hex-Rays"}
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -141,7 +141,7 @@ async def on_message(message):
         await message.channel.send(
             "Usage:\n"
             "1. Attach a file **MAX 2MB**.\n"
-            "2. Use `;revme` with decompiler names (e.g., `;revme binja/ghidra/hexrays/angr`) or no arguments to use all decompilers.\n"
+            "2. Use `;revme` with decompiler names (e.g., `;revme binja/ghidra/ida/angr`) or no arguments to use all decompilers.\n"
             "3. `;revme hex` for hex dump.\n"
             "4. `;revme asm` for disassembly.\n"
         )
