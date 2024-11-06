96fcdd7d42fb44764fd05aeefd584a88889effa2
1ikeadragon <40909973+1ikeadragon@users.noreply.github.com>
2024-11-06 09:44:26 +0530

    restard

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def send_file_or_text(message, filename, content, lang=""):
     if len(content) <= 2000:
         await message.channel.send(f"**{filename}**:\n```{lang}\n{content}\n```")
     else:
-        if "Hexdump" in filename or "Disassembly" in filename:
+        if "Hexdump" in filename:
             filename += ".txt"
         elif "asm" in lang:
             filename += ".asm"
